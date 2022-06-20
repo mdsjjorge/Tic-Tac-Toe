8e6bbff4e2c910908d7608325dbc960ce957e774
mdsjjorge <mdsousa.jorge@gmail.com>
2022-06-20 20:10:16 -0300

    conserta verificação

--- main.js
+++ main.js
@@ -1,70 +1,54 @@
-        
-        let squares = document.getElementsByClassName('square')
-        // let changeButton = document.getElementById('change')
-        let currentPlayer = 'X'
-        let res = document.getElementById('#res')
-        let arr = ['','','','','','','','','']
-        
-        function changePlayer () {
-            if (currentPlayer === 'X') {
-                currentPlayer = 'O'
-            } else {
-                currentPlayer = 'X'
-            }
+let squares = document.getElementsByClassName('square')
+let currentPlayer = 'X'
+let res = document.getElementById('#res')
+let winner = null
+let board = []
+let winningCombos = [ [0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [2,4,6] ]
+    
+  // [winningCombos[linha][coluna]]
+
+function changePlayer () {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O'
+    } else {
+        currentPlayer = 'X'
+    }
+}
+
+for (let i = 0; i < squares.length; i++) {
+    let square = squares[i]    
+    square.addEventListener('click', function () {
+        if (square.innerHTML === '') {
+            square.innerHTML = currentPlayer
+            updateBoard (i)
+            console.log(`board[${i}]= ${board[i]}`)
+            checkForWinner ()
+            changePlayer ()
         }
-        
-        // changeButton.addEventListener('click', changePlayer)
+    })    
+}
 
-        for (let i = 0; i < squares.length; i++) {
-            let square = squares[i]
-            // arr[i] = squares[i]
-            
-            square.addEventListener('click', function () {
-                if (square.innerHTML === '') {
-                    square.innerHTML = currentPlayer
-                    let pass = currentPlayer
-                    // square.value = currentPlayer
-                    changePlayer () 
-                    counter (i,pass)
-                    // console.log (i, pass)
-                    // console.log(`square[${i}]: ${square.value}`)
-                    // console.log(`squares[${i}]: ${squares[i]}`)
-                    // console.log(square[i])
-                }
-            })
-            
-        }
+const updateBoard = (i) => {
+    board[i] = currentPlayer
+}
 
-        function counter (i, pass) {
-            let counterX = pass
-            arr[i] = counterX
-            console.log(arr)
-            // if (arr[0] == arr[1] && arr[1] == arr[2]) {
-            //     res.innerHTML = 'alguem ganhou'
-            // }
-        }
 
-        // [0, 1, 2]
-        // [3, 4, 5]
-        // [6, 7, 8]
-        // [0, 3, 6]
-        // [1, 4, 7]
-        // [2, 5, 8]
-        // [0, 4, 8]
-        // [2, 4, 6]
-
-        // if (square[0] === 'X' && square[1] === 'X' && square[2] === 'X' ) {
-        //     res.innerHTML = 'X ganheu'
-        // }
-
-        // for (let i = 0; i < squares.length; i++) {
-        //     let square = squares[i]
-            
-        //     square.addEventListener('click', function () {
-        //         if (square.innerHTML === '') {
-        //             square.innerHTML = currentPlayer
-        //             changePlayer () 
-
-        //         }
-        //     })
-        // }
+function checkForWinner() {
+  for (var i=0; i < winningCombos.length; i++) {
+      if (board[winningCombos[i][0]] === currentPlayer && 
+          board[winningCombos[i][1]] === currentPlayer && 
+          board[winningCombos[i][2]] === currentPlayer) {
+      winner = currentPlayer;
+      console.log(`winner is ${winner}`)
+      return true;
+      }
+  }
+  return false;
+  }
